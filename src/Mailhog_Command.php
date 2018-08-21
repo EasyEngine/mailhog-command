953e85c95020059141e24ebe44530c8d539600be
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2018-08-21 18:10:15 +0530

    Update according to db refactoring
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

--- src/Mailhog_Command.php
+++ src/Mailhog_Command.php
@@ -16,9 +16,9 @@ use \Symfony\Component\Filesystem\Filesystem;
 class Mailhog_Command extends EE_Command {
 
 	/**
-	 * @var array $site Associative array containing essential site related information.
+	 * @var array $db Object containing essential site related information.
 	 */
-	private $site;
+	private $db;
 
 	/**
 	 * Enables mailhog on given site.
@@ -32,17 +32,19 @@ class Mailhog_Command extends EE_Command {
 
 		EE\Utils\delem_log( 'mailhog' . __FUNCTION__ . ' start' );
 		$args = EE\SiteUtils\auto_site_name( $args, 'mailhog', __FUNCTION__ );
-		$this->populate_site_info( $args );
-		chdir( $this->site['root'] );
+		$this->db = Site::find( EE\Utils\remove_trailing_slash( $args[0] ) );
+		if ( ! $this->db || ! $this->db->site_enabled ) {
+			EE::error( sprintf( 'Site %s does not exist / is not enabled.', $args[0] ) );
+		}
 
 		// TODO: mailhog_enabled fnuction after db changes
 		// if($this->mailhog_enabled()){
 		// 	EE::error('Mailhog is already enabled.');
 		// }
 
-		chdir( $this->site['root'] );
+		chdir( $this->db->site_fs_path );
 		$this->check_mailhog_available();
-		EE::docker()::docker_compose_up( $this->site['root'], [ 'mailhog' ] );
+		EE::docker()::docker_compose_up( $this->db->site_fs_path, [ 'mailhog' ] );
 		EE::exec( "docker-compose exec postfix postconf -e 'relayhost = mailhog:1025'" );
 
 	}
@@ -59,14 +61,16 @@ class Mailhog_Command extends EE_Command {
 
 		EE\Utils\delem_log( 'mailhog' . __FUNCTION__ . ' start' );
 		$args = EE\SiteUtils\auto_site_name( $args, 'mailhog', __FUNCTION__ );
-		$this->populate_site_info( $args );
+		$this->db = Site::find( EE\Utils\remove_trailing_slash( $args[0] ) );
+		if ( ! $this->db || ! $this->db->site_enabled ) {
+			EE::error( sprintf( 'Site %s does not exist / is not enabled.', $args[0] ) );
+		}
 
 		// TODO: mailhog_enabled fnuction after db changes
 		// if($this->mailhog_enabled()){
 		// 	Then only run this...
 		// }
 
-		chdir( $this->site['root'] );
 		$this->check_mailhog_available();
 		EE::docker()::stop_container( 'mailhog' );
 		EE::exec( 'docker-compose exec postfix postconf -e \'relayhost =\'' );
@@ -77,31 +81,14 @@ class Mailhog_Command extends EE_Command {
 	 * Function to check if mailhog is present in docker-compose.yml or not.
 	 */
 	private function check_mailhog_available() {
+
+		chdir( $this->db->site_fs_path );
 		$launch   = EE::launch( 'docker-compose config --services' );
 		$services = explode( PHP_EOL, trim( $launch->stdout ) );
 		if ( ! in_array( 'mailhog', $services, true ) ) {
-			EE::debug( 'Site type: ' . $this->site['type'] );
-			EE::debug( 'Site command: ' . $this->site['command'] );
+			EE::debug( 'Site type: ' . $this->db->site_type );
+			EE::debug( 'Site command: ' . $this->db->app_sub_type );
 			EE::error( sprintf( '%s site does not have support to enable/disable mailhog.' ) );
-		}
-	}
-
-	/**
-	 * Populate basic site info from db.
-	 */
-	private function populate_site_info( $args ) {
-
-		$this->site['name'] = EE\Utils\remove_trailing_slash( $args[0] );
-
-		if ( EE::db()::site_in_db( $this->site['name'] ) ) {
-
-			$db_select = EE::db()::select( [], [ 'sitename' => $this->site['name'] ], 'sites', 1 );
-
-			$this->site['type']    = $db_select['site_type'];
-			$this->site['root']    = $db_select['site_path'];
-			$this->site['command'] = $db_select['site_command'];
-		} else {
-			EE::error( sprintf( 'Site %s does not exist.', $this->site['name'] ) );
 		}
 	}
 
